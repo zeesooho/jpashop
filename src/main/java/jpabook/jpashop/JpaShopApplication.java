a912ec37efda97e905ab3135fc77740105996a7e
zeesooho <wltngh00@gmail.com>
2023-12-19 10:43:51 +0900

    오타 수정

--- src/main/java/jpabook/jpashop/JpaShopApplication.java
+++ src/main/java/jpabook/jpashop/JpaShopApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JpaBookApplication {
+public class JpaShopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JpaBookApplication.class, args);
+		SpringApplication.run(JpaShopApplication.class, args);
 	}
 
 }
