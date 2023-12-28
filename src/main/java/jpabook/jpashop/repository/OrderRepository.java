f156752e9f3d63b8a6d4ddf364d421a82329023f
zeesooho <wltngh00@gmail.com>
2023-12-29 02:08:50 +0900

    주문 레포지토리 추가

---
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -0,0 +1,23 @@
+package jpabook.jpashop.repository;
+
+import jakarta.persistence.EntityManager;
+import jpabook.jpashop.domain.Order;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+    private final EntityManager em;
+
+    public void save(Order order) {
+        em.persist(order);
+    }
+
+    public Order findOne(Long id) {
+        return em.find(Order.class, id);
+    }
+
+    // 나중에 추가
+//    public List<Order> findAll(OrderSearch orderSearch) {}
+}
