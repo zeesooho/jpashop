f89075d6b3b76c9bd7f4514e44f41e46a5ab59fe
zeesooho <wltngh00@gmail.com>
2023-12-29 00:38:42 +0900

    회원 서비스, 레포지토리 di 방법 변경

--- src/main/java/jpabook/jpashop/service/MemberService.java
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -3,6 +3,7 @@ package jpabook.jpashop.service;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.repository.MemberRepository;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,13 +11,10 @@ import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class MemberService {
 
     private final MemberRepository memberRepository;
-
-    public MemberService(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
 
     /**
      * 회원가입
