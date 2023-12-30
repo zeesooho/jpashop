654c983461122f4ad55cd9c99f0cbdbc8976e47f
zeesooho <wltngh00@gmail.com>
2023-12-30 23:41:39 +0900

    오타 수정

--- src/main/java/jpabook/jpashop/controller/MemberForm.java
+++ src/main/java/jpabook/jpashop/controller/MemberForm.java
@@ -5,9 +5,9 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter
-public class MembersForm {
+public class MemberForm {
 
-    @NotEmpty(message = "")
+    @NotEmpty(message = "회원 이름은 필수입니다.")
     private String name;
 
     private String city;
